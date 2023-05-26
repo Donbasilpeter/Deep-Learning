e527b916c1323c1005d523ca287c4530f75e8606
Don Basil Peter <donbasilpeter@gmail.com>
2023-05-25 21:51:01 -0700

    setup imports and preprocess data for ann training

---
+++ src/ANN/ANN.py
@@ -0,0 +1,8 @@
+# import neeeded libraries for the ANN
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+# preprocessing the given data for training purposes
+dataset = pd.read_csv('../../Data/Churn_Modelling.csv')
+X = dataset.iloc[:,3:-1].values
