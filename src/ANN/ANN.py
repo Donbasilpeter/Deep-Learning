8ee1c3c6ef9fac0ce803ac8165d5a17800576862
Don Basil Peter <donbasilpeter@gmail.com>
2023-05-25 22:09:12 -0700

    setting training data

--- src/ANN/ANN.py
+++ src/ANN/ANN.py
@@ -4,5 +4,7 @@ import pandas as pd
 import tensorflow as tf
 
 # preprocessing the given data for training purposes
-dataset = pd.read_csv('../../Data/Churn_Modelling.csv')
+dataset = pd.read_csv('./Data/Churn_Modelling.csv')
 X = dataset.iloc[:,3:-1].values
+Y = dataset.iloc[:,-1].values
+
